364a294a50d4f977d5c3a28d9d69f21c656672c3
Tao Wang <tao.wang@windriver.com>
2022-09-19 10:13:48 +0800

    update func

--- main.go
+++ main.go
@@ -5,28 +5,18 @@ import (
 	"os"
 
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
-	infrav1alpha1 "github.com/nephio-project/nephio-controller-poc/apis/infra/v1alpha1"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
 func Run(rl *fn.ResourceList) (bool, error) {
 
 	// find CfwConfig config
-	var cf *fn.KubeObject
-	for _, obj := range rl.Items {
-		if obj.IsGVK(infrav1alpha1.GroupVersion.Group, infrav1alpha1.GroupVersion.Version, "CfwConfig") {
-			//fmt.Printf("-------------%+v-----------\n", obj)
-			//fmt.Printf("-------------%s-----------\n", reflect.obj.Method(0))
-			cf = obj
-			continue
-		}
+	var cfw_config CfwConfig
+	if err := rl.FunctionConfig.As(&cfw_config); err != nil {
+		return false, fmt.Errorf("could not parse function config: %s", err)
 	}
-	if cf == nil {
-		return false, fmt.Errorf("could not find %s/%s CfwConfig",
-			infrav1alpha1.GroupVersion.Group, infrav1alpha1.GroupVersion.Version)
-	}
-	if err := applyCfwConfig(rl, cf); err != nil {
-		return false, err
-	}
+	//fmt.Printf("-------------%+v-----------\n", cf)
+	//return true, fmt.Errorf("-------------%+v-----------\n", cfw_config)
 	return true, nil
 }
 
@@ -36,11 +26,73 @@ func applyCfwConfig(rl *fn.ResourceList, cf *fn.KubeObject) error {
 		return fmt.Errorf("could not find %s/%s in CfwConfig",
 			"networkInfra", "type")
 	}
-
-	fmt.Printf("-------------%+v-----------\n", networkType)
-	cf.SetAnnotation("key", "avalue")
+	for _, obj := range rl.Items {
+		if obj.IsGVK("apps", "v1", "Deployment") {
+			//fmt.Printf("-------------%+v-----------\n", obj.GetAnnotation())
+		}
+	}
+	//fmt.Printf("-------------%+v-----------\n", networkType)
+	cf.SetAnnotation("akey", "avalue")
 	return nil
 }
+
+type CfwConfig struct {
+	yaml.ResourceIdentifier `json:",inline" yaml:",inline"`
+	ConfigMaps              []configList `json:"configMaps,omitempty" yaml:"configMaps,omitempty"`
+}
+
+type configList struct {
+	//type is coreFirewall or networkInfraCrd
+	PkgType      PkgType      `yaml:"pkgtype,omitempty" json:"pkgtype,omitempty"`
+	UpstreamLock UpstreamLock `yaml:"upstreamlock,omitempty" json:"upstreamlock,omitempty"`
+}
+
+// PkgType defines the type of cfirewall package usage.
+type PkgType string
+
+const (
+	// corefirewall and networkinfcrd specifies a package usage.
+	corefirewall  PkgType = "coreFirewall"
+	networkinfcrd PkgType = "networkInfraCrd"
+)
+
+// OriginType defines the type of origin for a package.
+type OriginType string
+
+const (
+	// GitOrigin specifies a package as having been cloned from a git repository.
+	GitOrigin OriginType = "git"
+)
+
+type UpstreamLock struct {
+	// Type is the type of origin.
+	OriginType OriginType `yaml:"origintype,omitempty" json:"origintype,omitempty"`
+
+	// Git is the resolved locator for a package on Git.
+	Git GitLock `yaml:"gitlock,omitempty" json:"gitlock,omitempty"`
+}
+
+type GitLock struct {
+	// Repo is the git repository that was fetched.
+	// e.g. 'https://github.com/kubernetes/examples.git'
+	Repo string `yaml:"repo,omitempty" json:"repo,omitempty"`
+
+	// Directory is the sub directory of the git repository that was fetched.
+	// e.g. 'staging/cockroachdb'
+	Directory string `yaml:"directory,omitempty" json:"directory,omitempty"`
+
+	// Ref can be a Git branch, tag, or a commit SHA-1 that was fetched.
+	// e.g. 'master'
+	Ref string `yaml:"ref,omitempty" json:"ref,omitempty"`
+
+	// Commit is the SHA-1 for the last fetch of the package.
+	// This is set by kpt for bookkeeping purposes.
+	Commit string `yaml:"commit,omitempty" json:"commit,omitempty"`
+
+	//Proxy is git proxy
+	Proxy string `yaml:"proxy,omitempty" json:"proxy,omitempty"`
+}
+
 func main() {
 	if err := fn.AsMain(fn.ResourceListProcessorFunc(Run)); err != nil {
 		os.Exit(1)
